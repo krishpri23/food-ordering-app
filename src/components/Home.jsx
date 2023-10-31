7ee52ac8c74c0d834f8a13e03e6740737105fe1d
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2023-10-30 21:14:09 -0700

    working api call

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { restaurants } from "../utils/mock";
-import fetchData from "../utils/api";
+import Shimmer from "../utils/shimmer";
+// import { restaurants } from "../utils/mock";
+// import fetchData from "../utils/api";
 
 export default function Home() {
   const [listOfRes, setListOfRes] = useState([]);
 
   console.log("body rendered");
 
+  const fetchData = async () => {
+    const res = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0826802&lng=80.2707184&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const data = await res.json();
+    setListOfRes(
+      data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    console.log(listOfRes);
+    console.log("inside fetch data ");
+  };
+
   useEffect(() => {
-    fetchData(setListOfRes);
+    fetchData();
+    console.log("inside use effcet");
   }, [setListOfRes]);
 
-  return (
+  return listOfRes.length === 0 ? (
+    <Shimmer />
+  ) : (
     <main className="res-cards">
       {listOfRes &&
         listOfRes.map((res) => {
-          console.log(res.info.name);
+          console.log("inside res body ");
           return (
             <div key={res.info.id} className="container">
               <h2> {res.info.name} </h2>
---
+++ src/utils/shimmer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Shimmer() {
+  return (
+    <div className="shimmer">
+      <h2> </h2>
+      <div>
+        <h3> </h3>
+        <h3> </h3>
+      </div>
+      <p> </p>
+    </div>
+  );
+}
