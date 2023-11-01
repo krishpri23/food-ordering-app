9dd541946087f58dd76c932c80c4bdf9263c2d0a
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2023-11-01 11:45:42 -0700

    search filter

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
+// using filterRes to display the api data to UI. Having listOfRes intact without making any changes. While filtering, it compares the filtered res with the actual res data
+
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import Shimmer from "../utils/shimmer";
-// import { restaurants } from "../utils/mock";
-// import fetchData from "../utils/api";
 
 export default function Home() {
   const [listOfRes, setListOfRes] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filterRes, setFilterRes] = useState([]);
 
   console.log("body rendered");
 
@@ -19,33 +21,67 @@ export default function Home() {
     setListOfRes(
       data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
+    setFilterRes(
+      data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
     console.log(listOfRes);
-    console.log("inside fetch data ");
   };
 
   useEffect(() => {
     fetchData();
-    console.log("inside use effcet");
   }, [setListOfRes]);
 
+  const handleFilter = () => {
+    try {
+      // when no filtering is done, it returns the actual array itself
+      const filteredRes = listOfRes.filter((res) =>
+        res.info.name.toLowerCase().includes(searchText.toLowerCase())
+      );
+      // debugger;
+      setFilterRes(filteredRes);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return listOfRes.length === 0 ? (
     <Shimmer />
   ) : (
-    <main className="res-cards">
-      {listOfRes &&
-        listOfRes.map((res) => {
-          console.log("inside res body ");
-          return (
-            <div key={res.info.id} className="container">
-              <h2> {res.info.name} </h2>
-              <div className="ratings">
-                <h3>{res.info.avgRating} star </h3>
-                <h3> {res.info.sla.deliveryTime} mins </h3>
+    <main>
+      <div className="search">
+        <input
+          type="text"
+          name="searchFilter"
+          id="searchFilter"
+          className="searchFilter"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+            console.log(searchText);
+          }}
+        />
+        <button
+          className="search-btn"
+          type="submit"
+          onClick={() => handleFilter()}
+        >
+          Search{" "}
+        </button>
+      </div>
+      <div className="res-cards">
+        {filterRes &&
+          filterRes.map((res) => {
+            return (
+              <div key={res.info.id} className="container">
+                <h2> {res.info.name} </h2>
+                <div className="ratings">
+                  <h3>{res.info.avgRating} star </h3>
+                  <h3> {res.info.sla.deliveryTime} mins </h3>
+                </div>
+                <p> {res.info.cuisines.join(", ")} </p>
               </div>
-              <p> {res.info.cuisines.join(", ")} </p>
-            </div>
-          );
-        })}
+            );
+          })}
+      </div>
     </main>
   );
 }
