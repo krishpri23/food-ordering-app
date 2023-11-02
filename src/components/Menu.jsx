fc518a0373400c91ed0ada9d1b4fa7fdbf449a44
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2023-11-01 23:26:49 -0700

    remove unused code

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,35 +2,35 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Menu() {
-  const [menu, setMenu] = useState([]);
+  const [menu, setMenu] = useState(["hello"]);
 
-  const fetchMenu = async () => {
-    const res = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0826802&lng=80.2707184&restaurantId=14096&catalog_qa=undefined&submitAction=ENTER"
-    );
+  //   const fetchMenu = async () => {
+  //     const res = await fetch(
+  //       "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0826802&lng=80.2707184&restaurantId=14096&catalog_qa=undefined&submitAction=ENTER"
+  //     );
 
-    const json = await res.json();
-    setMenu(json.data);
-  };
+  //     const json = await res.json();
+  //     setMenu(json.data);
+  //   };
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
+  //   useEffect(() => {
+  //     fetchMenu();
+  //   }, []);
 
-  if (menu) {
-    const { name, cuisines, costForTwoMessage } =
-      menu.cards[0]?.card?.card?.info || [];
-  }
+  //   if (menu) {
+  //     const { name, cuisines, costForTwoMessage } =
+  //       menu.cards[0]?.card?.card?.info || [];
+  //   }
 
+  console.log(useState());
   //   const itemCards =
   //     menu.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.itemCards;
   // menu.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
 
   return (
     <main>
-      <h2> {name} </h2>
-      <h4> {cuisines.join(",")}</h4>
-      <h4> {costForTwoMessage}</h4>
+      <h2> name</h2>
+      <h4>cuisines</h4>
     </main>
   );
 }
