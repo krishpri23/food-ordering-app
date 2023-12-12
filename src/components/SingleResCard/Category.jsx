5a665d56f2c8bb15daf6ca845f255b8732d38685
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2023-12-11 22:00:51 -0800

    adds count to each category

--- src/components/SingleResCard/Category.jsx
+++ src/components/SingleResCard/Category.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { FaStar } from "react-icons/fa";
+import { FaAngleDown } from "react-icons/fa6";
+import { FaAngleUp } from "react-icons/fa6";
 
 function RestaurantCategory(props) {
   // Returns all the arrays of cards that hold different categories
@@ -15,35 +17,69 @@ function RestaurantCategory(props) {
 
       {cards.map((card, index) => {
         const { title, itemCards, categories } = card?.card?.card;
-        console.log(categories);
 
         // To omit the card[0] as it has no title associated with it
         if (index !== 0) {
           return (
             <section className="flex flex-col gap-5 w-full my-5 " key={index}>
-              {/* Recommended Section  */}
+              {/* Recommended & Level 1 Headings */}
               <button
                 className="font-bold uppercase  py-4 text-start "
                 //   onClick={() => setOpenSection(!openSection)}
               >
-                {title}
+                {itemCards && itemCards?.length > 0
+                  ? `${title} (${itemCards.length})`
+                  : title}
               </button>
 
               {/* For all other categories */}
               {categories?.map((card, index) => {
                 const { title, itemCards } = card;
+                console.log(card);
                 return (
-                  <div key={index} className={`border-b-2 pb-3`}>
-                    {title} ({itemCards.length})
+                  <div key={index} className="border-b-2 pb-3">
+                    {itemCards.length > 0 ? (
+                      <>
+                        {/* Accordian - Category Heading Level 2*/}
+                        <div className="flex justify-between">
+                          <h1 className="font-bold uppercase">
+                            {title} ({itemCards.length})
+                          </h1>
+                          <button className=" cursor-pointer ">
+                            <FaAngleDown />
+                          </button>
+                        </div>
+
+                        {/* Category's heading's children has a list of items */}
+                        {itemCards?.map((card, index) => {
+                          const { name, price, description } = card?.card?.info;
+
+                          return (
+                            <div
+                              key={index}
+                              className="flex flex-col  px-5 py-4 w-full text-sm"
+                            >
+                              <h1>{name} </h1>
+                              <h1>Rs {price / 100}</h1>
+                              <p className="text-gray-500">{description}</p>
+                              <hr className="mt-6" />
+                            </div>
+                          );
+                        })}
+                      </>
+                    ) : (
+                      { title }({ itemCards })
+                    )}
                   </div>
                 );
               })}
 
-              {/* recommended cards */}
+              {/* recommended cards & other cards */}
               {itemCards?.map((item, index) => {
                 const { name, price, description, ribbon } = item?.card?.info;
+
                 return (
-                  <section key={index} className=" px-5 py-4 w-full">
+                  <section key={index} className=" px-5 py-4 w-full  text-sm">
                     {ribbon?.text && (
                       <div className="flex gap-2 items-center text-yellow-500">
                         <FaStar className="text-xs" />
