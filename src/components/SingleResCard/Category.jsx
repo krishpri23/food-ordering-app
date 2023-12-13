f51de93a8180c253501f811b894b8d47c57c5e5b
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2023-12-13 10:20:17 -0800

    added documentation for category comp

--- src/components/SingleResCard/Category.jsx
+++ src/components/SingleResCard/Category.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 import { FaAngleDown } from "react-icons/fa6";
 import { FaAngleUp } from "react-icons/fa6";
+
+/*
+ This component is the most confusing part of the website.
+
+ - cards
+  - itemCards - list of food items 
+    - categories (not available on every object)
+      - itemCards - list of food items
+
+*/
 
 function RestaurantCategory(props) {
   // Returns all the arrays of cards that hold different categories
   const { cards } = props?.resInfo?.cards[2].groupedCard?.cardGroupMap?.REGULAR;
   const { veg } = props?.resInfo.cards[0]?.card?.card?.info;
 
-  console.log(cards);
+  // State to open/close accordian
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+    console.log(open);
+  };
+  console.log("these are cards obj", cards);
   return (
     <section className=" w-full">
       <h1 className="w-full font-bold uppercase text-green-600 py-5">
@@ -17,40 +34,77 @@ function RestaurantCategory(props) {
 
       {cards.map((card, index) => {
         const { title, itemCards, categories } = card?.card?.card;
+        console.log(`Item cards of index ${index} `, itemCards);
+        console.log(`Categories : `, categories);
 
         // To omit the card[0] as it has no title associated with it
         if (index !== 0) {
           return (
             <section className="flex flex-col gap-5 w-full my-5 " key={index}>
-              {/* Recommended & Level 1 Headings */}
-              <button
-                className="font-bold uppercase  py-4 text-start "
-                //   onClick={() => setOpenSection(!openSection)}
-              >
-                {itemCards && itemCards?.length > 0
-                  ? `${title} (${itemCards.length})`
-                  : title}
-              </button>
+              <div className=" border-b-2 border-gray-500">
+                {/* Recommended & Level 1 Headings */}
+                <div
+                  className="flex justify-between items-center cursor-pointer"
+                  onClick={() => handleClick()}
+                >
+                  <div
+                    className="font-bold uppercase  py-4 text-start "
+                    //   onClick={() => setOpenSection(!openSection)}
+                  >
+                    {itemCards && itemCards?.length > 0
+                      ? `${title} (${itemCards.length})`
+                      : title}{" "}
+                  </div>
+                  <FaAngleDown />
+                </div>
 
-              {/* For all other categories */}
+                {/* Recommended and Level 1 Heading's children */}
+                {itemCards?.map((item, index) => {
+                  const { name, price, description, ribbon } = item?.card?.info;
+
+                  return (
+                    <section key={index} className=" px-5 py-4 w-full  text-sm">
+                      {ribbon?.text && (
+                        <div className="flex gap-2 items-center text-yellow-500">
+                          <FaStar className="text-xs" />
+                          <h1 className="text-yellow-600 font-bold">
+                            {" "}
+                            {ribbon?.text}
+                          </h1>
+                        </div>
+                      )}
+                      <h1> {name}</h1>
+                      <h1> Rs {price / 100}</h1>
+                      <p className="text-gray-500 my-2 pb-3 border-b-2 border-gray-200 ">
+                        {" "}
+                        {description}
+                      </p>
+                    </section>
+                  );
+                })}
+              </div>
+
+              {/* Available only for few cards not on all cards */}
               {categories?.map((card, index) => {
                 const { title, itemCards } = card;
                 console.log(card);
                 return (
                   <div key={index} className="border-b-2 pb-3">
-                    {itemCards.length > 0 ? (
+                    {itemCards.length > 0 && (
                       <>
                         {/* Accordian - Category Heading Level 2*/}
-                        <div className="flex justify-between">
+                        <div
+                          className="flex justify-between cursor-pointer"
+                          onClick={() => handleClick()}
+                        >
                           <h1 className="font-bold uppercase">
                             {title} ({itemCards.length})
                           </h1>
-                          <button className=" cursor-pointer ">
-                            <FaAngleDown />
-                          </button>
+
+                          <FaAngleDown />
                         </div>
 
-                        {/* Category's heading's children has a list of items */}
+                        {/* Category's (CLASSIC WRAP) heading's (VEG WRAPS) children (4 TYPES OF VEG WRAPS) has a list of items */}
                         {itemCards?.map((card, index) => {
                           const { name, price, description } = card?.card?.info;
 
@@ -59,6 +113,7 @@ function RestaurantCategory(props) {
                               key={index}
                               className="flex flex-col  px-5 py-4 w-full text-sm"
                             >
+                              <h2> hello </h2>
                               <h1>{name} </h1>
                               <h1>Rs {price / 100}</h1>
                               <p className="text-gray-500">{description}</p>
@@ -67,36 +122,10 @@ function RestaurantCategory(props) {
                           );
                         })}
                       </>
-                    ) : (
-                      { title }({ itemCards })
                     )}
                   </div>
                 );
               })}
-
-              {/* recommended cards & other cards */}
-              {itemCards?.map((item, index) => {
-                const { name, price, description, ribbon } = item?.card?.info;
-
-                return (
-                  <section key={index} className=" px-5 py-4 w-full  text-sm">
-                    {ribbon?.text && (
-                      <div className="flex gap-2 items-center text-yellow-500">
-                        <FaStar className="text-xs" />
-                        <h1 className="text-yellow-600 font-bold">
-                          {" "}
-                          {ribbon?.text}
-                        </h1>
-                      </div>
-                    )}
-                    <h1> {name}</h1>
-                    <h1> Rs {price / 100}</h1>
-                    <p className="text-gray-500 my-2 "> {description}</p>
-                    <hr className="mt-6" />
-                  </section>
-                );
-              })}
-              <hr />
             </section>
           );
         }
