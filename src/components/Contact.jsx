def9fe07d75a8e67bf52b1d945c14aca03e8c69c
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2023-12-18 10:33:38 -0800

    added another test case

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,6 +4,9 @@ export default function Contact() {
   return (
     <main>
       <h3> Contact us page </h3>
+      <input type="text" name="name" id="name" />
+      <input type="email" name="email" />
+      <button type="submit">Submit</button>
     </main>
   );
 }
--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,10 +2,21 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("contact component should load in the page", () => {
-  render(<Contact />);
+describe("Contact Us page test cases", () => {
+  test("contact component should load in the page", () => {
+    //rendering
+    render(<Contact />);
+    //querying
+    const heading = screen.getByRole("heading");
+    // assertion
+    expect(heading).toBeInTheDocument();
+  });
 
-  const heading = screen.getByRole("heading");
+  test("2 input boxes must be available in the component", () => {
+    render(<Contact />);
 
-  expect(heading).toBeInTheDocument();
+    const inputBoxes = screen.getAllByRole("textbox");
+
+    expect(inputBoxes.length).toBe(2);
+  });
 });
