3262bdbf608746256f7bfb9b30600a11475dcf86
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2024-01-31 17:13:51 -0800

    added login username in navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
 import cartSlice from "../utils/cartSlice";
 import { FaSearch } from "react-icons/fa";
 import { FaRegUserCircle } from "react-icons/fa";
 import { FaCartArrowDown } from "react-icons/fa";
+import { RxAvatar } from "react-icons/rx";
 
 const Header = () => {
+  const [login, setLogin] = useState("");
+
+  useEffect(() => {
+    setLogin(JSON.parse(localStorage.getItem("register")));
+    console.log(login);
+  }, []);
+
   // subscribing to the cart items
   const cartItems = useSelector((store) => store.cart.items);
   console.log("cart items", cartItems);
@@ -20,12 +28,25 @@ const Header = () => {
           </span>
           <li> Search </li>
         </NavLink>
-        <NavLink className="flex justify-center gap-3 items-center" to="/login">
-          <span>
-            <FaRegUserCircle />
-          </span>
-          <li> Sign In </li>
-        </NavLink>
+        {login ? (
+          <div className="flex justify-center items-center gap-3">
+            <span>
+              <RxAvatar />{" "}
+            </span>
+            <span className="text-black"> {login.name} </span>
+          </div>
+        ) : (
+          <NavLink
+            className="flex justify-center gap-3 items-center"
+            to="/login"
+          >
+            <span>
+              <FaRegUserCircle />
+            </span>
+            <li> Sign In </li>
+          </NavLink>
+        )}
+
         <NavLink
           className="flex justify-center gap-3 items-center"
           to="/contact"
