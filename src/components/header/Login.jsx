ab4f10244b8f3d602bf765fdb7f7f65931f84fbc
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2024-01-31 17:05:01 -0800

    added login using localstorage

--- src/components/header/Login.jsx
+++ src/components/header/Login.jsx
@@ -3,19 +3,34 @@ import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState();
+  const [emailValid, setEmailValid] = useState();
+
   const [password, setPassword] = useState();
+  const [passwordValid, setPasswordValid] = useState();
+
+  const [errMsg, setErrMsg] = useState();
+
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password);
-    email && password ? navigate("/") : "";
+    const login = JSON.parse(localStorage.getItem("register"));
+    console.log("Logged in successfully");
+    login.email === email && login.password === password
+      ? navigate("/")
+      : setErrMsg(" Invalid Email or Password !!");
   };
 
   return (
     <main className="w-full pt-10">
       <h1 className="font-bold text-center text-3xl"> Login </h1>
       <form className="w-1/3 bg-slate-200 p-10" onSubmit={handleSubmit}>
+        {errMsg && (
+          <p className="font-semibold text-red-500 -my-6 text-center">
+            {" "}
+            {errMsg}{" "}
+          </p>
+        )}
         <input
           type="email"
           id="email"
@@ -31,12 +46,11 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
         <p className="font-thin text-slate-900 text-sm -mt-7">
-          {" "}
           By clicking on Login, I accept the Terms & Conditions & Privacy Policy
         </p>
-        <button id="primary-btn" type="submit">
-          {" "}
-          Login{" "}
+
+        <button className="primary-btn" type="submit">
+          Login
         </button>
 
         <Link
