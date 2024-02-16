ff3dd38cea5fd4da534acdf3abbf0bd29bc4fbe1
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2024-02-16 09:53:48 -0800

    added some changes

--- src/app.jsx
+++ src/app.jsx
@@ -1,99 +1,35 @@
 import ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./components/Navbar/Login";
 import Home from "./components/Home";
-import {
-  Outlet,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
-
-import RestaurantMenu from "./components/RestaurantMenu";
-import { Provider } from "react-redux";
-import appStore from "./utils/appStore";
-import { Register } from "./components/Navbar/Register";
-import { SearchRestaurant } from "./components/Navbar/SearchRestaurant";
-import Error from "./components/home/Error";
-
-import {
-  Cart,
-  Login,
-  Register,
-  SearchRestaurant,
-} from "./components/Navbar/index.js";
-
-import Header from "./components/home/Header";
+import Cart from "./components/Navbar/Cart";
+import Register from "./components/Navbar/Register";
+import SearchRestaurant from "./components/Navbar/SearchRestaurant";
 import DetailedSliderOption from "./components/home/DetailedSliderOption";
-const appRouter = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<App />}>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/search" element={<SearchRestaurant />} />
-      <Route path="/restaurants/:resId" element={<RestaurantMenu />} />
-      <Route
-        path="//collections/:itemName/:itemId"
-        element={<DetailedSliderOption />}
-      />
-    </Route>
-  )
-
-  // [
-  //   {
-  //     path: "/",
-  //     element: <App />,
-  //     errorElement: <Error />,
-  //     children: [
-  //       {
-  //         path: "/",
-  //         element: <Home />,
-  //       },
-  //       {
-  //         path: "/login",
-  //         element: <Login />,
-  //       },
-  //       {
-  //         path: "/register",
-  //         element: <Register />,
-  //       },
-
-  //       {
-  //         path: "/search",
-  //         element: <SearchRestaurant />,
-  //       },
-
-  //       {
-  //         path: "/restaurants/:resId",
-  //         element: <RestaurantMenu />,
-  //       },
-  //       {
-  //         path: "/collections/:itemName/:itemId",
-  //         element: <DetailedSliderOption />,
-  //       },
-  //       {
-  //         path: "/cart",
-  //         element: <Cart />,
-  //       },
-  //     ],
-  //   },
-  // ]
-);
-
-function App() {
-  return (
-    <>
-      {/* To use redux inside react app, react uses provider as a bridge */}
-      <Provider store={appStore}>
-        <Header />
-        <Outlet />
-      </Provider>
-    </>
-  );
-}
-
-export default App;
+import RestaurantMenu from "./components/RestaurantMenu";
+import { CartProvider } from "./context/CartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={appRouter} />);
+root.render(
+  <React.StrictMode>
+    <CartProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/search" element={<SearchRestaurant />} />
+          <Route path="/restaurants/:resId" element={<RestaurantMenu />} />
+          <Route
+            path="//collections/:itemName/:itemId"
+            element={<DetailedSliderOption />}
+          />
+        </Routes>
+      </Router>
+    </CartProvider>
+  </React.StrictMode>
+);
